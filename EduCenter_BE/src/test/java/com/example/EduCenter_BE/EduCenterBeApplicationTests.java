c139320dc8b5362aea76158f7e7fd93258105dac
trantiendat12345 <trantiendatas1@gmail.com>
2025-12-04 23:57:34 +0700

    first commit

---
+++ EduCenter_BE/src/test/java/com/example/EduCenter_BE/EduCenterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EduCenter_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduCenterBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
