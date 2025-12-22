167f1bd2a4fb81d77bb9888707b8f77bf38234cf
trantiendat12345 <trantiendatas1@gmail.com>
2025-12-22 11:04:48 +0700

    [V17_BE] Format JSON start date

--- EduCenter_BE/src/main/java/com/example/EduCenter_BE/request/CreateClassroomRequest.java
+++ EduCenter_BE/src/main/java/com/example/EduCenter_BE/request/CreateClassroomRequest.java
@@ -1,6 +1,7 @@
 package com.example.EduCenter_BE.request;
 
 import com.example.EduCenter_BE.constant.enums.ClassroomStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -8,6 +9,7 @@ import java.time.LocalDate;
 @Data
 public class CreateClassroomRequest {
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
     private Integer maxStudent;
     private String name;
